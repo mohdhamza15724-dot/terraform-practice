3587a5890e33eb799d09e4b6fabe80db109a09b4
mohdhamza15724-dot <mohdhamza15724@gmail.com>
2026-03-05 12:31:03 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "preprocess_ec2" {
- ami           = "ami-056335ec4a8783947"
+ ami           = "ami-0c94855ba95c71c99"
  instance_type = "t3.micro"
  vpc_security_group_ids = [
 aws_security_group.abc_sg.id
